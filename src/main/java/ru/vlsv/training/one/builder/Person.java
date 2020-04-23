763325458c0d5d702bc58bf3bfcc0293719d1b17
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-04-23 15:09:38 +0300

    Training one

---
+++ src/main/java/ru/vlsv/training/one/builder/Person.java
@@ -0,0 +1,12 @@
+package ru.vlsv.training.one.builder;
+
+/**
+ * GeekBrains Java, training.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 23.04.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class Person {
+}
---
+++ src/main/java/ru/vlsv/training/one/errors/FixCars.java
@@ -0,0 +1,83 @@
+package ru.vlsv.training.one.errors;
+
+/**
+ * GeekBrains Java, training.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 23.04.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class Errors {
+
+    interface Moveable{
+        void move();
+    }
+
+    interface Stopable{
+        void stop();
+    }
+
+    abstract class Car{
+        public Engine engine;
+        private String color;
+        private String name;
+
+
+        protected void start(){
+            System.out.println("Car starting");
+        }
+
+        abstract void open();
+
+        public Engine getEngine() {
+            return engine;
+        }
+
+        public void setEngine(Engine engine) {
+            this.engine = engine;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    class LightWeightCar extends Car implements Moveable{
+
+        @Override
+        void open() {
+            System.out.println("Car is open");
+        }
+
+        @Override
+        public void move() {
+            System.out.println("Car is moving");
+        }
+
+    }
+
+    class Lorry extends Car, Moveable, Stopable{
+
+        public void move(){
+            System.out.println("Car is moving");
+        }
+
+        public void stop(){
+            System.out.println("Car is stop");
+        }
+    }
+
+}
